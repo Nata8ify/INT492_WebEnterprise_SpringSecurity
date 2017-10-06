8285c20c2ac4e47f61e70c5d1e669fc87fe85617
NATTAWUT PICHAIWATTNPHON <nata8ify@gmail.com>
2017-10-06 22:13:42 +0700

    MAKE A Simple Roles (ROLE_ADMIN, ROLE_USER)

---
+++ src/main/java/com/arms/user/UserController.java
@@ -0,0 +1,25 @@
+package com.arms.user;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+	
+	@Secured("ROLE_USER")
+	@ResponseBody
+	@RequestMapping("/user/user")
+	public String user(){
+		return "Only ROLE_USER can access this page";
+	}
+
+	@Secured("ROLE_ADMIN")
+	@ResponseBody
+	@RequestMapping("/admin/user")
+	public String admin(){
+		return "Only ROLE_AMIN can access this page";
+	}
+
+}
